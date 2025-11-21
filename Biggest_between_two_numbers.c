86a8aad4bf4862e2ac16178a0475e73c6acca78e
25kq1a5406 <25kq1a5406pace.in.ac@gmail.com>
2025-11-21 20:00:13 +0530

    Create Biggest_between_two_numbers.c

---
+++ Biggest_between_two_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the values of a,b");
+scanf("%d%d",&a,&b);
+if(a>b)
+{
+printf("a is big number");
+}
+else
+{
+printf("b is big number");
+}
+return 0;
+}
