a081a5700f680fac5b321c010b3def01da6141b6
25kq1a5406 <25kq1a5406pace.in.ac@gmail.com>
2025-11-27 23:14:21 +0530

    Create Auto storage.c

---
+++ Auto storage.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     auto int mainvar;
+     void printnum();
+     mainvar=10;
+     printf("\n in main:%d",mainvar);
+}
+void printnum()
+{
+   auto int mainvar;
+   printf("%d ",mainvar);
+}
