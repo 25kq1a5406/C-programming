2cd8fe554a39565fa9aa81ba66351cca79eb6f8e
25kq1a5406 <25kq1a5406pace.in.ac@gmail.com>
2025-11-29 00:20:46 +0530

    Call by value.c

---
+++ Call by value.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a;
+   void printnum(int);
+   a=10;
+   printf("\n in main a=%d",a);
+   printnum(a);
+   printf("\n in main end a=%d",a);
+} 
+void printnum(int a)
+{
+  a++;
+  printf("\n in printnum fun a=%d",a);
+}   
